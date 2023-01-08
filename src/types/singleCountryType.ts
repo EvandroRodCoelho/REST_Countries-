ac6db7a364cfcda656fee652e273c0e5f11ace6a
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-01-07 22:19:01 -0300

    Type

--- src/types/singleCountryType.ts
+++ src/types/singleCountryType.ts
@@ -1,21 +1,21 @@
 export interface ISingleCountry {
-  name: string,
-  nativeName: string,
-  population: number,
-  region: string,
-  subregion: string,
-  capital: string,
-  topLevelDomain: string,
-  languages: [{
+  name?: string,
+  nativeName?: string,
+  population?: number,
+  region?: string,
+  subregion?: string,
+  capital?: string,
+  topLevelDomain?: string,
+  languages?: [{
     iso639_1: string;
     iso639_2: string;
     name: string;
     nativeName: string;
   }],
-  flag: string,
-  borders: [string];
-  alpha2Code: string;
-  currencies: [{
+  flag?: string,
+  borders?: [string];
+  alpha2Code?: string;
+  currencies?: [{
     code: string;
     name: string;
     symbol: string;
